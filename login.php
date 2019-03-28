2ae198eeb731439433888af682a3c6170de4cd03
besmith67 <39537167+besmith67@users.noreply.github.com>
2019-03-27 23:27:28 -0400

    Update login.php

--- login.php
+++ login.php
@@ -8,12 +8,13 @@
 // Start or resume session, and create: $_SESSION[] array
 session_start(); 
 require '../database/database.php';
+$labelError = "";
 if ( !empty($_POST)) { // if $_POST filled then process the form
 	// initialize $_POST variables
 	$username = $_POST['username']; // username is email address
 	$password = $_POST['password'];
 	$passwordhash = MD5($password);
-	$labelError = "";
+	
 		
 	// verify the username/password
 	$pdo = Database::connect();
